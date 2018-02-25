55cd434da138d5972f4a564809f8bcc319770c1b
Kewen Leite <kewenleite@gmail.com>
2018-02-25 15:33:28 -0300

    primeiro commit

---
+++ CaelumEstoque/Controllers/CategoriaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            CategoriasDAO categoriasDAO = new CategoriasDAO();
+            IList<CategoriaDoProduto> listaCategorias = categoriasDAO.Lista();
+            ViewBag.listaCategorias = listaCategorias;
+            return View();
+        }
+    }
+}
